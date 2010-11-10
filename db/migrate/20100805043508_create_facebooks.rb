052ce611fb23292478fcf2a5bba6ad74875ab269
nov matake <nov@matake.jp>
2010-11-10 17:52:18 +0900

    make fb user id string since it exceeds integer max

--- db/migrate/20100805043508_create_facebooks.rb
+++ db/migrate/20100805043508_create_facebooks.rb
@@ -1,7 +1,7 @@
 class CreateFacebooks < ActiveRecord::Migration
   def self.up
     create_table :facebooks do |t|
-      t.integer :identifier, :unsigned => true, :limit => 20
+      t.string :identifier, :unsigned => true, :limit => 20
       t.string :access_token
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,11 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
@@ -12,7 +13,7 @@
 ActiveRecord::Schema.define(:version => 20100805043508) do
 
   create_table "facebooks", :force => true do |t|
-    t.integer  "identifier",   :limit => 20
+    t.string   "identifier",   :limit => 20
     t.string   "access_token"
     t.datetime "created_at"
     t.datetime "updated_at"
