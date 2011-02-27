9c29482575f9a174c9a2548019ba73ccf2e817cb
nov matake <nov@matake.jp>
2011-02-27 16:22:25 +0900

    typo

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,6 +1,6 @@
 class CanvasController < ApplicationController
   def create
-    auth = lFacebook.auth.from_signed_request(params)
+    auth = Facebook.auth.from_signed_request(params)
     user = auth.user.fetch
     render :text => "Hello #{user.name}!"
   end
