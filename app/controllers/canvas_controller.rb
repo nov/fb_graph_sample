d7752e8e019485bcd10634ee70d28d9fa1fdc1f6
nov matake <nov@matake.jp>
2011-02-27 19:38:01 +0900

    put signed_request params only

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,6 +1,6 @@
 class CanvasController < ApplicationController
   def create
-    auth = Facebook.auth.from_signed_request(params)
+    auth = Facebook.auth.from_signed_request(params[:signed_request])
     user = auth.user.fetch
     render :text => "Hello #{user.name}!"
   end
