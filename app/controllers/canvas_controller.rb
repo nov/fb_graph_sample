d2af1ab14195702874dc22f31efa9467ca3ecbb0
nov matake <nov@matake.jp>
2011-02-27 19:53:50 +0900

    handle unauthorized canvas

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,7 +1,11 @@
 class CanvasController < ApplicationController
   def create
     auth = Facebook.auth.from_signed_request(params[:signed_request])
-    user = auth.user.fetch
-    render :text => "Hello #{user.name}!"
+    if auth.authorized?
+      user = auth.user.fetch
+      render :text => "Hello #{user.name}!"
+    else
+      render :text => "Hello Anonymous!"
+    end
   end
 end
