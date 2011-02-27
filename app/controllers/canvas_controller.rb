f220aac628dabc04aa2cda6172753807e305f8b3
nov matake <nov@matake.jp>
2011-02-27 16:20:39 +0900

    hello @user

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,5 +1,7 @@
 class CanvasController < ApplicationController
   def create
-    render :text => params.inspect
+    auth = lFacebook.auth.from_signed_request(params)
+    user = auth.user.fetch
+    render :text => "Hello #{user.name}!"
   end
 end
