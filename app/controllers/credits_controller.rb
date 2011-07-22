97e6cd1e5f0478f112757ba88edeb12bb71e9573
nov matake <nov@matake.jp>
2011-07-22 17:05:04 +0900

    setup response for payments_get_items call

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,6 +1,22 @@
 class CreditsController < ApplicationController
   def create
     # TODO
-    render :nothing => true
+    auth = Facebook.auth.from_signed_request(params[:signed_request])
+    transaction = auth.data[:credits]
+    case transaction[:method]
+    when 'payments_get_items'
+      render :json => {
+        :content => [{
+          :title => "[Test Mode] Unicorn",
+          :description => "[Test Mode] Own your own mythical beast!",
+          :price => 2,
+          :image_url => "http://www.facebook.com/images/gifts/21.png",
+          :product_url => "http://www.facebook.com/images/gifts/21.png"
+        }],
+        :method => "payments_get_items"
+      }
+    when 'payments_status_update'
+      render :nothing => true
+    end
   end
 end
